2da0ac318f58c425d5a23f1f83c36382f18283c9
Reece Russell <reece@prominentmedia.com>
2020-07-02 13:08:38 +0100

    added content file

---
+++ src/content.js
@@ -0,0 +1,16 @@
+export default {
+	socials: [
+		{
+			type: "instagram",
+			link: "https://instagram.com",
+		},
+		{
+			type: "twitter",
+			link: "https://twitter.com",
+		},
+		{
+			type: "facebook",
+			link: "https://facebook.com",
+		},
+	],
+};
